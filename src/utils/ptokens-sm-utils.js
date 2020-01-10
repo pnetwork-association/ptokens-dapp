99dc9c1de14ecf6c680156ffa6d21db78092a9de
allemanfredi <alle.manfredi@gmail.com>
2020-01-10 14:04:35 +0100

    chore(global): updates enclave pubkey and pbtc sc

--- src/utils/ptokens-sm-utils.js
+++ src/utils/ptokens-sm-utils.js
@@ -3,8 +3,8 @@ import settings from '../settings'
 const PEOS_ETH_SMART_CONTRACT_ADDRESS = '0x4AEAFc6F72eD16665a70A45297500a0BD9d8c2F0'
 const PEOS_EOS_SMART_CONTRACT_ADDRESS = 'provabletokn'
 
-const PBTC_ETH_SMART_CONTRACT_ADDRESS = '0xb8b4194c6b81e9f2333f4b8a98cd86228e3e84d5'
-const PBTC_ENCLAVE_PUBKEY = '0x03afb21281fa1524c8605d23e825a84a50c7a2b94891115c22e34e0d2c67f95220'
+const PBTC_ETH_SMART_CONTRACT_ADDRESS = '0xb070c3e0e4af9b125880a4a6ab0000ea0ebf06c5'
+const PBTC_ENCLAVE_PUBKEY = '02e40fdbfd5e2dd5f99d33b9687c249dfe0412a3960a898e36c8754caabf2f37ad'
 
 const getCorrespondingSmartContractAddress = (_pTokenName, _role) => {
   switch (_pTokenName) {
