dfaf06c7f9f8fddf48afe3317e113a0a6b786656
allemanfredi <alle.manfredi@gmail.com>
2020-01-14 13:19:59 +0100

    chore(utils): updates enc btc pub key and eth sc

--- src/utils/ptokens-sm-utils.js
+++ src/utils/ptokens-sm-utils.js
@@ -3,8 +3,8 @@ import settings from '../settings'
 const PEOS_ETH_SMART_CONTRACT_ADDRESS = '0x4AEAFc6F72eD16665a70A45297500a0BD9d8c2F0'
 const PEOS_EOS_SMART_CONTRACT_ADDRESS = 'provabletokn'
 
-const PBTC_ETH_SMART_CONTRACT_ADDRESS = '0xafead8e831b15be5913680aef90141a3ffe53f16'
-const PBTC_ENCLAVE_PUBKEY = '0x036b55adbce2b881552980743484e8b6f034d3e6398c039c9e020f5485efe5b5eb'
+const PBTC_ETH_SMART_CONTRACT_ADDRESS = '0xf8641d84bad037f0df40abe2051191e193f809c9'
+const PBTC_ENCLAVE_PUBKEY = '03e2ae724cdb5add655ab97bbbc35611ce53b962467230a06c3ec742480ac31662'
 
 const getCorrespondingSmartContractAddress = (_pTokenName, _role) => {
   switch (_pTokenName) {
