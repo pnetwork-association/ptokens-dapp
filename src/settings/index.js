288aa21ecf05163be5e4c9f81992b57c0df50b64
allemanfredi <alle.manfredi@gmail.com>
2020-01-28 17:42:16 +0100

    chore(settings): updates GA tracking code

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,7 +1,7 @@
 const settings = {
   dappName: 'pTokens Dapp',
   telegram: 'https://t.me/ptokens',
-  googleAnalyticsTrackId: 'UA- 154250265-1', 
+  googleAnalyticsTrackId: 'UA-157173999-1', 
   peos: {
     eth: {
     wsInfuraEndpoint: 'wss://kovan.infura.io/ws/v3/',
