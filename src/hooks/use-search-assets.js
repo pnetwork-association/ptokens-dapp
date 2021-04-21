2366c815c219cc22885c65ae09603eb92983609f
Alessandro Manfredi <alle.manfredi@gmail.com>
2021-04-21 13:25:34 +0200

    fix(components): fixes asset search

--- src/hooks/use-search-assets.js
+++ src/hooks/use-search-assets.js
@@ -6,8 +6,11 @@ const useSearchAssets = _assets => {
   const [assets] = useMemo(() => {
     return [
       _assets.filter(
-        ({ name, blockchain, address }) =>
+        ({ name, nativeBlockchain, blockchain, address, symbol, coin }) =>
           name.toLowerCase().includes(searchWord.toLowerCase()) ||
+          symbol.toLowerCase().includes(searchWord.toLowerCase()) ||
+          (coin && coin.toLowerCase().includes(searchWord.toLowerCase())) ||
+          nativeBlockchain.toLowerCase().includes(searchWord.toLowerCase()) ||
           `${name} on ${blockchain}`.toLowerCase().includes(searchWord.toLowerCase()) ||
           (address && address.toLowerCase() === searchWord.toLowerCase())
       )
