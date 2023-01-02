8bd78d445b0975342c0f5d175e5a4e684b571753
Alain Olivier <oliviera9@gmail.com>
2023-01-02 09:22:32 +0100

    refactor: set peg-in fee for GALA_ON_BSC_MAINNET to 0.1%

--- src/utils/fee.js
+++ src/utils/fee.js
@@ -11,11 +11,6 @@ const getFee = (_from, _to) => {
       return 0
     }
   }
-  switch (_to.id) {
-    case GALA_ON_BSC_MAINNET: {
-      return 0
-    }
-  }
   if (_from.isNative) return map.pegin
   else if (!_from.isNative && !_to.isNative) return map.pegin
   else return map.pegout
