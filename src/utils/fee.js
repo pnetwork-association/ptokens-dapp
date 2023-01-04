038876ce49f8ee0aae664d53fcf2a81091ce273a
Alain Olivier <oliviera9@gmail.com>
2023-01-04 16:31:44 +0100

    refactor(fees): set pGALA fees to 100 bps for peg-in and 250 bps for peg-out

--- src/utils/fee.js
+++ src/utils/fee.js
@@ -1,16 +1,9 @@
-import { GALA_ON_BSC_MAINNET } from '../constants'
-
 const map = {
   pegin: 0.1,
   pegout: 0.25
 }
 
 const getFee = (_from, _to) => {
-  switch (_from.id) {
-    case GALA_ON_BSC_MAINNET: {
-      return 0
-    }
-  }
   if (_from.isNative) return map.pegin
   else if (!_from.isNative && !_to.isNative) return map.pegin
   else return map.pegout
