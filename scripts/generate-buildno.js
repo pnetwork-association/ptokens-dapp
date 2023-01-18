eab5b2970f77544b859caff202d0de892c91e9a1
eenvin <envin@proton.me>
2023-01-18 15:11:58 +0100

    feat: add commit version to footer

---
+++ scripts/generate-buildno.js
@@ -0,0 +1,18 @@
+const childProcess = require("child_process")
+const fs = require("fs")
+const os = require("os")
+
+function setEnvValue(key, value) {
+  const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
+  const target = ENV_VARS.indexOf(ENV_VARS.find((line) => {
+      return line.match(new RegExp(key))
+  }));
+  ENV_VARS.splice(target, 1, `${key}=${value}`)
+  fs.writeFileSync("./.env", ENV_VARS.join(os.EOL))
+}
+
+(() => {
+  childProcess.exec("git rev-parse --short HEAD", (err, stdout) => {
+    setEnvValue("REACT_APP_GIT_SHA", stdout)
+  });
+})();
