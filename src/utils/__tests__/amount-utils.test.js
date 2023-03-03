dfeb569602eb588fdfffda97c5028b93ca2a3815
eenvin <envin@proton.me>
2023-03-03 20:08:35 +0100

    chore: replace jest with vitest

--- src/utils/__tests__/amount-utils.test.js
+++ src/utils/__tests__/amount-utils.test.js
@@ -1,42 +1,52 @@
+import { afterEach, test, vi, describe, expect } from 'vitest';
 import { getDecimalSeparator, getThousandSeparator, formatDecimalSeparator } from '../amount-utils'
 
 describe('getDecimalSeparator', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   test.each([
     [',', 'it-IT'],
     ['.', 'en-EN'],
     ['.', 'fr-FR'],
     ['.', 'fa-IR'],
     ['.', 'uz-Cyrl-UZ']
-  ])('Should return %p when locale is %p', (_separator, _locale) => {
-    jest.spyOn(Intl, 'NumberFormat').mockReturnValue(new Intl.NumberFormat(_locale))
+  ])('Should return %s when locale is %s', (_separator, _locale) => {
+    vi.spyOn(Intl, 'NumberFormat').mockReturnValue(new Intl.NumberFormat(_locale))
     const sep = getDecimalSeparator()
     expect(sep).toEqual(_separator)
   })
 })
 
 describe('getThousandSeparator', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   test.each([
     ['.', 'it-IT'],
     [',', 'en-EN'],
     [',', 'fr-FR'],
     [',', 'fa-IR'],
     [',', 'uz-Cyrl-UZ']
-  ])('Should return %p when locale is %p', (_separator, _locale) => {
-    jest.spyOn(Intl, 'NumberFormat').mockReturnValue(new Intl.NumberFormat(_locale))
+  ])('Should return %s when locale is %s', (_separator, _locale) => {
+    vi.spyOn(Intl, 'NumberFormat').mockReturnValue(new Intl.NumberFormat(_locale))
     const sep = getThousandSeparator()
     expect(sep).toEqual(_separator)
   })
 })
 
 describe('formatDecimalSeparator', () => {
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
   test.each([
     ['54325,28', 'it-IT'],
     ['54325.28', 'en-EN'],
     ['54325.28', 'fr-FR'],
     ['54325.28', 'fa-IR'],
     ['54325.28', 'uz-Cyrl-UZ']
-  ])('Should return %p when locale is %p', (_expectedResult, _locale) => {
-    jest.spyOn(Intl, 'NumberFormat').mockReturnValue(new Intl.NumberFormat(_locale))
+  ])('Should return %s when locale is %s', (_expectedResult, _locale) => {
+    vi.spyOn(Intl, 'NumberFormat').mockReturnValue(new Intl.NumberFormat(_locale))
     const num = 54325.28
     const formattedNum = formatDecimalSeparator(num)
     expect(formattedNum).toEqual(_expectedResult)
