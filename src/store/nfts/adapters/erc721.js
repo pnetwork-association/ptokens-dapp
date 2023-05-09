acb22743f8145f27c6d77f9221a74dbaf3dd23a0
Alain Olivier <oliviera9@gmail.com>
2023-05-09 14:58:50 +0200

    refactor: remove unnecessary new Promise from loadErc721Data()

--- src/store/nfts/adapters/erc721.js
+++ src/store/nfts/adapters/erc721.js
@@ -9,46 +9,38 @@ const loadErc721Data = async ({ nfts, account, web3, dispatch }) => {
     const erc721s = nfts.map(({ contractAddress }) => new web3.eth.Contract(ERC721Abi, contractAddress))
 
     await Promise.all(
-      erc721s.map(
-        (_erc721, _index) =>
-          new Promise(async _resolve => {
-            try {
-              const totalbalance = await _erc721.methods.balanceOf(account).call()
+      erc721s.map(async (_erc721, _index) => {
+        const totalbalance = await _erc721.methods.balanceOf(account).call()
 
-              const ids = await Promise.all(
-                [...Array(BigNumber(totalbalance).toNumber()).keys()].map((_, _index) =>
-                  _erc721.methods.tokenOfOwnerByIndex(account, _index).call()
-                )
-              )
+        const ids = await Promise.all(
+          [...Array(BigNumber(totalbalance).toNumber()).keys()].map((_, _index) =>
+            _erc721.methods.tokenOfOwnerByIndex(account, _index).call()
+          )
+        )
 
-              const data = await Promise.all(
-                ids.map(
-                  _id =>
-                    new Promise((_resolve, _reject) =>
-                      axios
-                        .get(`${nfts[_index].baseUri}/${_id}`)
-                        .then(({ data }) => _resolve(data))
-                        .catch(_reject)
-                    )
-                )
+        const data = await Promise.all(
+          ids.map(
+            (_id) =>
+              new Promise((_resolve, _reject) =>
+                axios
+                  .get(`${nfts[_index].baseUri}/${_id}`)
+                  .then(({ data }) => _resolve(data))
+                  .catch(_reject)
               )
+          )
+        )
 
-              dispatch(
-                nftsDataLoaded(
-                  ids.map((_id, _index) => ({
-                    ...nfts[0],
-                    ...data[_index],
-                    id: _id,
-                    balance: BigNumber(1)
-                  }))
-                )
-              )
-            } catch (_err) {
-              console.error(_err)
-            }
-            _resolve()
-          })
-      )
+        dispatch(
+          nftsDataLoaded(
+            ids.map((_id, _index) => ({
+              ...nfts[0],
+              ...data[_index],
+              id: _id,
+              balance: BigNumber(1),
+            }))
+          )
+        )
+      })
     )
   } catch (_err) {
     console.error(_err)
