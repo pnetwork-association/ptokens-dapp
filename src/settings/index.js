e12482899c3a099bf29bf035b3ae3419169fea55
Alain Olivier <oliviera9@gmail.com>
2023-05-16 12:56:13 +0200

    refactor: remove dotenv
    Vite already loads .env file by default.

--- src/settings/index.js
+++ src/settings/index.js
@@ -1,13 +1,6 @@
-// import * as dotenv from 'dotenv'
-
-// const loadGAnalyticsID = () => {
-//   dotenv.config()
-//   return import.meta.env.VITE_GOOGLE_ANALITYCS_TRACK_ID
-// }
-
 const settings = {
   dappName: 'pTokens Dapp',
-  // googleAnalyticsTrackId: loadGAnalyticsID(),
+  googleAnalyticsTrackId: import.meta.env.VITE_GOOGLE_ANALYTICS_TRACKING_ID,
   links: {
     audit: 'https://skynet.certik.com/projects/pnt',
     stats: 'https://pnetwork.watch/',
