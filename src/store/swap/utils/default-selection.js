08a5b43b0ab17b826663334f51aa924515545c3e
Alain Olivier <oliviera9@gmail.com>
2023-06-27 18:45:34 +0300

    feat: add USDC on XDAI and Arbitrum

--- src/store/swap/utils/default-selection.js
+++ src/store/swap/utils/default-selection.js
@@ -6,8 +6,8 @@ const getDefaultSelection = (_assets, _options = {}) => {
 }
 
 const getDefaultSelectionV2 = (_assets, { asset, from, to, host_symbol }) => {
-  const tkn = _assets.find(({ id }) => id === 'TKN')
-  const ptkn = _assets.find(({ id }) => id === 'PTKN_ON_GOERLI_TESTNET')
+  const tkn = _assets.find(({ id }) => id === 'USDC_ON_XDAI')
+  const ptkn = _assets.find(({ id }) => id === 'PUSDC_ON_ARBITRUM_MAINNET')
   const assetFrom = asset
     ? _assets.find(
         ({ isPtoken, nativeSymbol, blockchain, symbol }) =>
@@ -29,15 +29,15 @@ const getDefaultSelectionV2 = (_assets, { asset, from, to, host_symbol }) => {
     toastr.error('Error', 'Invalid routing')
   }
 
-  if (assetFrom && assetTo) {
-    const pTokenDefaultFrom = Object.assign({}, assetFrom)
-    const pTokenDefaultTo = Object.assign({}, assetTo)
+  const pTokenDefaultFrom = Object.assign({}, assetFrom ? assetFrom : tkn)
+  const pTokenDefaultTo = Object.assign({}, assetTo ? assetTo : ptkn)
+
+  if (pTokenDefaultFrom && pTokenDefaultTo) {
     pTokenDefaultFrom.defaultFrom = true
     pTokenDefaultTo.defaultTo = true
-    return [pTokenDefaultFrom, pTokenDefaultTo]
   }
 
-  return [tkn, ptkn]
+  return [pTokenDefaultFrom, pTokenDefaultTo]
 }
 
 export { getDefaultSelection, getDefaultSelectionV2 }
