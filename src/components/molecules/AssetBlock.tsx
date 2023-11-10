d210917de82c0218652eb85e04c6fe44732bd628
eenvin <envin@proton.me>
2023-11-10 21:43:33 +0100

    feat(assetFilter): filter assets when searching in modal

--- src/components/molecules/AssetBlock.tsx
+++ src/components/molecules/AssetBlock.tsx
@@ -1,20 +1,22 @@
 import { Blockchain } from 'ptokens-constants'
 import swapAssets, { getAllNativeAssets, isHost, isNative } from '../../constants/swap-assets'
 import { Asset, HostAsset } from '../../constants/swap-assets'
+import { getChainByBlockchain } from '../../constants/swap-chains'
 
 type AssetBoxProps = {
   setAsset: (arg0: Asset) => void
   isOpen: (arg0: boolean) => void
+  searchWord?: string
 }
 
-const AssetBlock = ({ setAsset, isOpen }: AssetBoxProps): JSX.Element => {
+const AssetBlock = ({ setAsset, isOpen, searchWord = '' }: AssetBoxProps): JSX.Element => {
   return(
-    <div className='mt-6'>
+    <div className='mt-6 rounded-md'>
     {Object.values(getAllNativeAssets())
       .map((nativeAsset: Asset) => (
-      <div key={nativeAsset.id} className="collapse collapse-arrow bg-base-200 mt-2">
+      <div key={nativeAsset.id} className="collapse collapse-arrow bg-base-200 mt-2 rounded-md">
         <input type="checkbox" name="my-accordion-1" /> 
-        <div className="collapse-title text-xl font-medium flex items-center">
+        <div className="collapse-title text-xl font-medium flex items-center text-slate-100">
           <img src={`/svg/${nativeAsset.image}`} className='w-10 mr-3' />
           {nativeAsset.symbol}
         </div>
@@ -25,10 +27,18 @@ const AssetBlock = ({ setAsset, isOpen }: AssetBoxProps): JSX.Element => {
                 singleAsset.symbol === nativeAsset.symbol) ||
               (isHost(singleAsset) &&
                 (singleAsset as HostAsset).nativeSymbol === nativeAsset.symbol))
+            .filter(
+              ({ name, blockchain, symbol }) =>
+                name.toLowerCase().includes(searchWord.toLowerCase()) ||
+                symbol.toLowerCase().includes(searchWord.toLowerCase()) ||
+                Blockchain[blockchain].toLowerCase().includes(searchWord.toLowerCase()) ||
+                `${symbol.toLowerCase()} on ${Blockchain[blockchain]}`.toLowerCase().includes(searchWord.toLowerCase()) ||
+                `${name} on ${Blockchain[blockchain]}`.toLowerCase().includes(searchWord.toLowerCase())
+            )
             .map((asset: Asset) => (
               <button 
                 key={asset.id}
-                className='btn flex flex-row justify-start items-center w-full mb-2' 
+                className='btn btn-lg text-sm hover:bg-blue-900 text-slate-200 flex flex-row justify-start items-center align-top w-full mb-2 rounded-md' 
                 onClick={() => {
                   setAsset(asset)
                   isOpen(false)
@@ -36,7 +46,15 @@ const AssetBlock = ({ setAsset, isOpen }: AssetBoxProps): JSX.Element => {
               >
                 <img src={`/svg/${asset.image}`} className='w-10 mr-3' />
                 {asset.symbol}
-                <p className='ml-auto'>on {Blockchain[asset.blockchain]}</p>
+                <div className='ml-auto flex flex-row'>
+                    {isNative(asset) ? 
+                      (<div className='badge badge-outline badge-accent mr-2 rounded-md !lines-color'> NATIVE </div>) :
+                      (<div className='badge badge-outline badge-accent mr-2 rounded-md !lines-color'> PTOKEN </div>)
+                    }
+                    ON
+                    {/* {Blockchain[asset.blockchain]} */}
+                    <img className='w-5 h-5 ml-2' src={`/svg/${getChainByBlockchain(asset.blockchain).image}`}></img>
+                </div>
               </button>
             ))}
         </div>
--- src/components/organisms/AssetsModal.tsx
+++ src/components/organisms/AssetsModal.tsx
@@ -1,6 +1,7 @@
 import cn from "classnames"
 import { Asset } from "../../constants/swap-assets"
 import AssetBlock from "../molecules/AssetBlock"
+import { useState } from "react"
 
 type AssetsModalProps = {
   setAsset: (arg0: Asset) => void
@@ -9,23 +10,24 @@ type AssetsModalProps = {
 }
 
 const AssetsModal = ({ setAsset, open, isOpen }: AssetsModalProps): JSX.Element => {
+  const [searchWord, setSearchWord] = useState('')
   const modalClass = cn({
-    "modal modal-bottom sm:modal-middle": true,
+    "modal modal-bottom sm:modal-middle !fixed top-0 left-0": true,
     "modal-open": open,
   })
+
+  const changeSearchWord = (event: any) => {
+    if (event?.target.value)
+      setSearchWord(event.target.value)
+  }
+
   return(
     <dialog id="my_modal_1" className={modalClass}>
       <form method="dialog" className="modal-box !rounded-md !pt-2">
-        <button className="btn btn-sm btn-circle btn-ghost absolute text-lg right-4 top-2" onClick={() => isOpen(false)}>✕</button>
-        <h2 className="font-bold text-xl mb-2 first-letter:">Asset search</h2>
-        <div className="border border-gray-600 rounded-md">
-
-          <div className="ml-4 mt-2">Chains:</div>
-          <div className="flex justify-center align-middle">
-            <input type="text" placeholder="Search by name, symbol or asset" className="input text-lg h-8 w-full m-4 mt-2 bg-gray-700" />
-          </div>
-        </div>
-        <AssetBlock setAsset={setAsset} isOpen={isOpen}/>
+        <button className="btn btn-sm btn-circle btn-ghost absolute text-lg right-4 top-2 text-slate-100" onClick={() => isOpen(false)}>✕</button>
+        <h2 className="font-bold text-xl mb-2 text-slate-100">Asset search</h2>
+        <input type="text" placeholder="Search by name, symbol or asset" className="input text-lg h-8 w-full mt-2 bg-gray-700 text-slate-100" onChange={changeSearchWord} />
+        <AssetBlock setAsset={setAsset} isOpen={isOpen} searchWord={searchWord} />
       </form>
     </dialog>
   )
