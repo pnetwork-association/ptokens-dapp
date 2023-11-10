13b4e2989f684e273234356b7f004cc2a628d39c
eenvin <envin@proton.me>
2023-11-10 21:31:34 +0100

    feat(assetChart): add support for different timeframes on the chart

--- src/components/molecules/AssetChart.tsx
+++ src/components/molecules/AssetChart.tsx
@@ -2,6 +2,23 @@ import Chart from 'react-apexcharts'
 import swapAssets, { Asset, HostAsset, NativeAsset, isNative } from '../../constants/swap-assets'
 import { SetStateAction, useEffect, useState } from 'react'
 import axios, { AxiosError } from 'axios'
+import cn from 'classnames'
+import { RiArrowRightDownLine, RiArrowRightUpLine } from 'react-icons/ri'
+
+interface AssetChartProps  {
+  asset: Asset
+  width?: number
+  height?: number
+}
+
+type TimeUnit = 'y' | 'm' | 'w' | 'd'
+
+type GainLoss = 'Gain' | 'Loss' | 'NoData'
+
+type GainLossT = {
+  amount: string
+  type: GainLoss
+}
 
 const getNativeAsset = (asset: Asset) => {
   if (isNative(asset))
@@ -10,17 +27,60 @@ const getNativeAsset = (asset: Asset) => {
     return Object.values(swapAssets).find((currAsset: Asset) => currAsset.id === (asset as HostAsset).underlyingAsset) as NativeAsset
 }
 
-interface AssetChartProps  {
-  asset: Asset
-}
+const computeGainLoss = (from: number, to: number) => (to - from) / to * 100
 
-const AssetChart = ({asset}: AssetChartProps): JSX.Element => {
+const AssetChart = ({asset, width = 700, height = 320}: AssetChartProps): JSX.Element => {
+  const [timeFrame, setTimeFrame] = useState<TimeUnit>('w')
+  const [numberOfDays, setNumberOfDays] = useState<number>(7)
   const [series, setSeries] = useState([{
       name: 'series-1',
       data: []
     }])
   const [dataPointIndex, setDataPointIndex] = useState(-1)
   const [value, setValue] = useState('')
+  const [lastValue, setLastValue] = useState<string>('N/A')
+  const [firstValue, setFirstValue] = useState<string>('N/A')
+  const [gains, setGains] = useState<string | GainLossT>('-')
+  
+  const btn1Year = cn({
+    "btn btn-xs btn-outline btn-accent ml-2": true,
+    "btn-active": timeFrame === 'y',
+  })
+  
+  const btn1Month = cn({
+    "btn btn-xs btn-outline btn-accent ml-2": true,
+    "btn-active": timeFrame === 'm',
+  })
+  
+  const btn1Week = cn({
+    "btn btn-xs btn-outline btn-accent ml-2": true,
+    "btn-active": timeFrame === 'w',
+  })
+  
+  const btn1Day = cn({
+    "btn btn-xs btn-outline btn-accent ml-2": true,
+    "btn-active": timeFrame === 'd',
+  })
+
+  const gainLoss = cn({
+    "mr-5 mt-2": true,
+    "text-green-500": (typeof gains === 'object' && gains.type === 'Gain'),
+    "text-red-500": (typeof gains === 'object' && gains.type === 'Loss'),
+  })
+
+  const getGainLoss = (from: string, to: string): GainLossT | string => {
+    const gainLoss = (Number(from) && Number(to)) ? computeGainLoss(Number(from), Number(to)) : '-'
+    if (typeof gainLoss === 'number' && gainLoss >= 0)
+      return {amount: gainLoss.toString().slice(0,4).concat('%'), type: 'Gain'}
+    else if (typeof gainLoss === 'number' && gainLoss < 0)
+      return {amount: gainLoss.toString().slice(1,5).concat('%'), type: 'Loss'}
+    else
+      return {amount: gainLoss.toString(), type: 'NoData'}
+  }
+
+  useEffect(() => {
+    setGains(getGainLoss(firstValue, lastValue))
+  }, [firstValue, lastValue])
   
   useEffect(() => {
     if (dataPointIndex !== -1)
@@ -28,10 +88,25 @@ const AssetChart = ({asset}: AssetChartProps): JSX.Element => {
   }, [dataPointIndex])
 
   useEffect(() => {
+    const days = timeFrame === 'y' ? 365 : timeFrame === 'm' ? 30 : timeFrame === 'w' ? 7 : timeFrame === 'd' ? 1 : 7
+    setNumberOfDays(days)
+  }, [timeFrame])
+
+  useEffect(() => {
+    const dataArrayLength = series[0].data.length
+    if (dataArrayLength > 0 && typeof series[0].data[dataArrayLength - 1][1] === 'number') {
+      setFirstValue(series[0].data[0][1])
+      setLastValue(series[0].data[dataArrayLength - 1][1])
+    } 
+    else
+      setLastValue('N/A')
+  }, [series[0].data])
+
+  useEffect(() => {
     const loadData = async (url: string, precision: number) => {
       const params = {
         vs_currency: 'usd',
-        days: 1,
+        days: numberOfDays,
         precision: precision,
       };
       try {
@@ -50,7 +125,7 @@ const AssetChart = ({asset}: AssetChartProps): JSX.Element => {
       }
     }
     loadData(getNativeAsset(asset).marketApi, 4)
-  }, [asset])
+  }, [asset, numberOfDays])
 
   const options: ApexCharts.ApexOptions = {
     chart: {
@@ -165,20 +240,44 @@ const AssetChart = ({asset}: AssetChartProps): JSX.Element => {
 
   return(
     <>
-      <div className="flex justify-start mx-5 mt-5">
-        <img
-            className="w-8"
-            src={getNativeAsset(asset) ? `/svg/${getNativeAsset(asset).image}` : '/svg/blockchain.svg'}
-          />
-        <h1 className='ml-2 text-2xl font-medium'>
-          {getNativeAsset(asset).symbol}
-        </h1>
+      <div className="flex justify-between">
+        <div className="flex justify-start mx-5 mt-5">
+          <img
+              className="w-8"
+              src={getNativeAsset(asset) ? `/svg/${getNativeAsset(asset).image}` : '/svg/blockchain.svg'}
+            />
+          <h1 className='ml-2 text-2xl font-medium text-slate-100'>
+            {getNativeAsset(asset).symbol}
+          </h1>
+        </div>
+        <div className='mx-5 mt-5'>
+          <button className={btn1Year} onClick={() => setTimeFrame('y')}>1y</button>
+          <button className={btn1Month} onClick={() => setTimeFrame('m')}>1m</button>
+          <button className={btn1Week} onClick={() => setTimeFrame('w')}>1w</button>
+          <button className={btn1Day} onClick={() => setTimeFrame('d')}>1d</button>
+        </div>
       </div>
-      <h2 className='mt-2 text-4xl font-medium mx-5'>
-        {value ? value : 'N/A'}
-      </h2>
+      <div className="flex justify-between">
+        <h2 className='mt-2 text-4xl font-medium mx-5 text-slate-100'>
+          {value ? value : lastValue}
+        </h2>
+        <div className={gainLoss}>
+          {typeof gains === 'object' ? (
+            <div className='flex align-middle'>
+              <div className='mt-1'>
+                {gains.type === 'Loss' ? (
+                  <RiArrowRightDownLine />
+                ) : gains.type === 'Gain' ? (
+                  <RiArrowRightUpLine />
+                ) : null}
+              </div>
+              {gains.amount}
+            </div>
+          ) : gains}
+        </div>
+      </div>
       <div className='mx-2'>
-        <Chart options={options} series={series} type="line" width={700} height={320} />
+        <Chart options={options} series={series} type="line" width={width} height={height} />
       </div>
     </>
   )
