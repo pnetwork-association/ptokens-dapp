18b5ac32aea7c624f66120d4faff0d0dd9cd466c
eenvin <envin@proton.me>
2023-11-10 21:32:17 +0100

    feat(copy): add copy button to asset addresses

--- src/components/molecules/InfoCard.tsx
+++ src/components/molecules/InfoCard.tsx
@@ -1,5 +1,6 @@
 import { Blockchain } from "ptokens-constants"
 import { pTokensAsset } from "ptokens-entities"
+import { RiFileCopyLine } from 'react-icons/ri'
 
 import { Asset, NativeAsset, isNative } from "../../constants/swap-assets"
 import { getBlockchain, getPrettierAddress } from "../../utils/utils"
@@ -20,9 +21,9 @@ const InfoCard = ({asset, pTokenAsset, title, className = ''}: InfoCardProps): J
     NO_ADDRESS
 
   return(
-    <div className={`flex justify-start items-center bg-base-100 rounded-md ml-5 mr-5 my-2 pb-2 w-1/2 ${className}`}>
+    <div className={`flex justify-start items-center bg-base-100 rounded-md lg:mx-5 max-lg:mx-0 my-2 pb-2 md:w-1/2 max-md:w-9/12 ${className}`}>
       <div className="flex flex-col">
-      <h1 className="text-md m-2 text-gray-500">{title}</h1>
+      <h1 className="text-md m-2 text-slate-200">{title}</h1>
         <div className="flex items-center ml-4">
           <img
             className="w-8 mr-2 mb-2"
@@ -31,7 +32,7 @@ const InfoCard = ({asset, pTokenAsset, title, className = ''}: InfoCardProps): J
           <div className='mb-2 text-2xl'>{asset.symbol}</div>
         </div>
         <div className="flex items-center ml-4">
-          <div className='bold mr-2 w-16 text-gray-500'>CHAIN:</div>
+          <div className='bold mr-2 w-16 text-slate-300'>CHAIN:</div>
           <img
             className="w-4 mx-2"
             src={`/svg/${getBlockchain(asset).image}`}
@@ -39,17 +40,20 @@ const InfoCard = ({asset, pTokenAsset, title, className = ''}: InfoCardProps): J
           <div>{Blockchain [asset.blockchain].toUpperCase()}</div>
         </div>
         <div className="flex items-center ml-4">
-          <div className='bold mr-2 w-16 text-gray-500'>TYPE:</div>
+          <div className='bold mr-2 w-16 text-slate-300'>TYPE:</div>
           {isNative(asset) ? 
-            (<div className='badge badge-outline badge-primary ml-2 rounded-md'> NATIVE </div>) :
-            (<div className='badge badge-outline badge-primary ml-2 rounded-md'> PTOKEN </div>)
+            (<div className='badge badge-outline badge-accent ml-2 rounded-md'> NATIVE </div>) :
+            (<div className='badge badge-outline badge-accent ml-2 rounded-md'> PTOKEN </div>)
           }
         </div>
         <div className="flex items-center ml-4">
-          <div className='bold mr-2 w-16 text-gray-500'>ADDRESS:</div>
+          <div className='bold mr-2 w-16 text-slate-300'>ADDRESS:</div>
           <a className='link link-info ml-2' href={getCorrespondingTokenExplorerLinkByBlockchain(asset.blockchain, address)}>
             {(address === NO_ADDRESS) ? 'No address found' : getPrettierAddress(address, 4)}
           </a>
+          <button className="ml-2 btn btn-outline btn-accent btn-xs" onClick={() => {navigator.clipboard.writeText(address)}}>
+            <RiFileCopyLine />
+          </button>
         </div>
       </div>
     </div>
