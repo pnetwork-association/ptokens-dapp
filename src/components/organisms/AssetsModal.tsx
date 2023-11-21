8e6de02fb6bd4766f233c5ff7e29d1805053c3d1
eenvin <envin@proton.me>
2023-11-21 15:51:42 +0100

    refactor(color): update bg color

--- src/components/organisms/AssetsModal.tsx
+++ src/components/organisms/AssetsModal.tsx
@@ -23,10 +23,10 @@ const AssetsModal = ({ setAsset, open, isOpen }: AssetsModalProps): JSX.Element 
 
   return(
     <dialog id="my_modal_1" className={modalClass}>
-      <form method="dialog" className="modal-box !rounded-md !pt-2">
+      <form method="dialog" className="modal-box !rounded-md !pt-2 bg-base-200 border border-base-300">
         <button className="btn btn-sm btn-circle btn-ghost absolute text-lg right-4 top-2 text-slate-100" onClick={() => isOpen(false)}>✕</button>
         <h2 className="font-bold text-xl mb-2 text-slate-100">Asset search</h2>
-        <input type="text" placeholder="Search by name, symbol or asset" className="input text-lg h-8 w-full mt-2 bg-gray-700 text-slate-100" onChange={changeSearchWord} />
+        <input type="text" placeholder="Search by name, symbol or asset" className="input text-lg h-8 w-full mt-2 bg-base-100 text-slate-100" onChange={changeSearchWord} />
         <AssetBlock setAsset={setAsset} isOpen={isOpen} searchWord={searchWord} />
       </form>
     </dialog>
