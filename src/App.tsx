36514312f06e7a29f629055071f580fc8b962b17
eenvin <envin@proton.me>
2024-01-09 12:59:22 +0100

    feat(feedback): add feedback popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import { RiFeedbackLine } from "react-icons/ri"
 
 import './App.css'
 import Navbar from './components/organisms/Navbar'
@@ -16,6 +17,16 @@ const App = (): JSX.Element => {
         <Route path="/risks" element={<Risks />} />
         <Route path="/" element={<Navigate to="/swap" replace />} />
       </Routes>
+      <div className="toast">
+        <div className="alert rounded-lg">
+          <button className='flex' data-tally-open="3yJOxg" data-tally data-tally-align-left="1" data-tally-hide-title="1" data-tally-overlay="1" data-tally-auto-close="1000">
+            <RiFeedbackLine size={'24'}/> 
+            <div className='ml-2'>
+              Feedback? Let us know!
+            </div>
+          </button>
+        </div>
+      </div>
     </Router>
   )
 }
